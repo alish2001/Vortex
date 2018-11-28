85e6de73e5ae1d8dad6b3824e7055946c757f4ee
Ali Shariatmadari <ali.shariat2001@gmail.com>
2018-11-28 13:52:54 -0500

    Added Basic Keyboard Input and Screen Offset Support

--- src/exige/supply/vortex/renderer/Renderer.java
+++ src/exige/supply/vortex/renderer/Renderer.java
@@ -1,17 +1,23 @@
 package exige.supply.vortex.renderer;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-import java.nio.Buffer;
+
+import javax.swing.JFrame;
+
+import exige.supply.vortex.input.Keyboard;
 
 public class Renderer extends Canvas implements Runnable {
 
-    public final static int scale = 5;
-    public final static int width = 300;
-    public final static int height = width / 16 * 9;
+	private static final long serialVersionUID = 1L;
+	
+	public final static int SCALE = 3;
+    public final static int WIDTH = 300;
+    public final static int HEIGHT = WIDTH / 16 * 9;
 
     public final static double UP = 60;
 
@@ -21,7 +27,9 @@ public class Renderer extends Canvas implements Runnable {
     private boolean running;
 
     private Screen screen;
-    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // Create image
+    private Keyboard keys;
+    
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB); // Create image
     private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData(); // Access image from image raster
 
     public Renderer() {
@@ -35,8 +43,11 @@ public class Renderer extends Canvas implements Runnable {
     }
 
     private void init(){
-        screen = new Screen(width, height);
-        Dimension size = new Dimension(width * scale, height * scale);
+        screen = new Screen(WIDTH, HEIGHT);
+        keys = new Keyboard();
+        addKeyListener(keys); // Enable keyboard input
+        
+        Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
         setPreferredSize(size); // Set window dimensions
         frame = new JFrame();
         frame.setResizable(false); // Disable resize
@@ -66,8 +77,8 @@ public class Renderer extends Canvas implements Runnable {
                 updates++; // Increment update counter
                 delta--; // Decrement delta, if delta is still >= wait for the update() to catch up
             }
-            render();
-            frames++;
+            render(); // Render frame
+            frames++; // Increment frames per frame render
 
             if (System.currentTimeMillis() - timer > 1000) { // Once a second has passed
                 timer += 1000; // Increment timer by one second
@@ -92,7 +103,7 @@ public class Renderer extends Canvas implements Runnable {
         }
 
         screen.clear(); // Clear screen
-        screen.render(); // Render current screen
+        screen.render(0, 0); // Render current screen
 
         for (int i = 0; i < pixels.length; i++) {
             pixels[i] = screen.getPixels()[i]; // Write screen to buffered image
--- src/exige/supply/vortex/renderer/Screen.java
+++ src/exige/supply/vortex/renderer/Screen.java
@@ -4,41 +4,49 @@ import java.util.Random;
 
 public class Screen {
 
-    private int width, height;
-    private int[] pixels;
+	private int width, height;
+	private int[] pixels;
 
-    private int[] tiles = new int[4096]; // Map tiles array 64 * 64
+	public final int MAP_SIZE = 64;
+	public final int MAP = MAP_SIZE * MAP_SIZE;
+	public final int MAP_MASK = MAP_SIZE - 1;
+	
+	private int[] tiles = new int[MAP]; // Map tiles array
 
-    private Random random = new Random();
+	private Random random = new Random();
 
-    public Screen(int width, int height) {
-        this.width = width;
-        this.height = height;
-        pixels = new int[this.width * this.height]; // Create pixel array
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[this.width * this.height]; // Create pixel array
 
-         for (int i = 0; i < 4096; i++){
-             tiles[i] = random.nextInt(0xffffff); // Generate random color from white to black spectrum
-         }
+		for (int i = 0; i < MAP; i++) {
+			tiles[i] = random.nextInt(0xffffff); // Generate random color from white to black spectrum
+		}
 
-    }
+	}
 
-    public void clear() {
-        for (int i = 0; i < pixels.length; i++) {
-            pixels[i] = 0;
-        }
-    }
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
 
-    public void render() {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int tileIndex = (x / 32) + (y / 32) * 64; // Extrapolate tile index from screen pixels | x / 16 is x shifted to the right by 5
-                pixels[x + y * width] = tiles[tileIndex];
-            }
-        }
+	public void render(int xOffset, int yOffset) {
+		for (int y = 0; y < height; y++) {
+			int yy = y + yOffset;
+			for (int x = 0; x < width; x++) {
+				int xx = x + xOffset;
+				// Extrapolate tile index from screen pixels | x / 16 is x shifted to the right
+				// by 4 | bitwise &, when the result of the shift is above 63, bring back to 0
+				int tileIndex = ((xx >> 4) & MAP_MASK) + ((yy >> 4) & MAP_MASK) * MAP_SIZE;
+				pixels[x + y * width] = tiles[tileIndex];
+			}
+		}
 
-    }
+	}
 
-    public int[] getPixels() {
-        return pixels;
-    }
+	public int[] getPixels() {
+		return pixels;
+	}
 }
